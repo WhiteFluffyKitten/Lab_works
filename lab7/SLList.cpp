450a34d7feaf02cebfe47ba9bc47b09999acb75d
WhiteFluffyKitten <64683093+WhiteFluffyKitten@users.noreply.github.com>
2020-12-13 12:56:27 +0500

    lab7

---
+++ lab7/SLList.cpp
@@ -0,0 +1,52 @@
+#include "SLList.h"
+
+SLList::SLList() {};
+
+SLList:: ~SLList()
+{
+	if (hasNext())
+		delete next();
+}
+
+int SLList::getData() const
+{
+	return m_data;
+}
+
+void SLList::setData(int data)
+{
+	m_data = data;
+}
+
+SLList* SLList::next() const
+{
+	return m_nextElem;
+}
+bool SLList::hasNext() const
+{
+	return (m_nextElem != nullptr);
+}
+SLList* SLList::beginList(int data)
+{
+	auto list = new SLList();
+	list->setData(data);
+	return list;
+}
+SLList* SLList::addElement(int data)
+{
+	if (hasNext()) return nullptr;
+	m_nextElem = new SLList();
+	setData(data);
+	return m_nextElem;
+}
+void  SLList::removeNext()
+{
+	if (hasNext())
+	{
+		auto n = next()->next();
+		next()->m_nextElem = nullptr;
+		delete next();
+		m_nextElem = n;
+	}
+}
+
