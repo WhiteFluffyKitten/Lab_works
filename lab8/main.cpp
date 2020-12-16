df46edaf7fc640ef4ac8b4985e279b91d0965692
WhiteFluffyKitten <64683093+WhiteFluffyKitten@users.noreply.github.com>
2020-12-16 09:15:31 +0500

    Update main.cpp

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -6,7 +6,7 @@ int main()
 	int m = 5;
 	auto maze = new Maze(n, m);
 	
-	for (int i = 0; i < maze->GetN(); i++)
+	for (int i = 0; i < n; i++)
 		for (int j = 0; j < 2; j++)
 		{
 			maze->makeConnection(i, i+j,i+j,i+1);
